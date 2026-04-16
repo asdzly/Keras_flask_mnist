edad90f011df9e8885ccaaa8338bfb7ff2544a1b
asdzly <ershiyiiii@qq.com>
2026-04-17 01:40:59 +0800

    主程序

--- app.py
+++ app.py
@@ -12,8 +12,6 @@ import tensorflow.keras as keras
 from flask import Flask, render_template, request, jsonify
 from tensorflow.keras.preprocessing.image import img_to_array, load_img
 
-# 使用 redis 统计总访问次数，今日访问次数from redis_util import get_today, get_visit_num_all, get_visit_num_today, inc_visit_num
-
 app = Flask(__name__)
 
 model_file = './model/model.h5'
@@ -22,7 +20,6 @@ model = keras.models.load_model(model_file)
 
 @app.route('/')
 def index():
-    # inc_visit_num()
     response = {
         'code': 0,
         'visits_all': 10086,              # 假数据：总访问次数
@@ -33,7 +30,6 @@ def index():
 
 @app.route('/predict/', methods=['Get', 'POST'])
 def preditc():
-    # inc_visit_num()  # 每访问一次，增加访问次数
     parseImage(request.get_data())
     img = img_to_array(load_img('output.png', target_size=(28, 28), color_mode="grayscale")) / 255.
     img = np.expand_dims(img, axis=0)
@@ -47,21 +43,10 @@ def preditc():
     print(response)
     return jsonify(int(code))
 
-'''
-def get_visit_info(code=0):
-    response = {}
-    response['code'] = code
-    response['visits_all'] = get_visit_num_all()
-    response['visits_today'] = get_visit_num_today()
-    response['today'] = get_today()
-    return response
-'''
 def parseImage(imgData):
     imgStr = re.search(b'base64,(.*)', imgData).group(1)
     with open('./output.png', 'wb') as output:
         output.write(base64.decodebytes(imgStr))
     
-
-
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=3335)
